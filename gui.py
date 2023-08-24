11a7cf4823e24461887c00595a7c61d1eca9c44a
i5-PC <mcienkowski@gmail.com>
2023-08-24 22:29:09 +0200

    Implement a dictionary of items (stone, paper, scissors)

--- gui.py
+++ gui.py
@@ -1,15 +1,17 @@
 import PySimpleGUI as sg
 
+dictionary_of_items = {1 : "stone", 2 : "paper", 3 : "scissors"}
+
 sg.theme("DarkAmber")
 
 label = sg.Text("Choose stone, paper or scissors")
 stone_button = sg.Button("Stone", key="stone")
-paper_button = sg.Button("Pape", key="paper")
+paper_button = sg.Button("Paper", key="paper")
 scissors_button = sg.Button("Scissors", key="scissors")
 
 layout = [[label], [[stone_button, paper_button, scissors_button]]]
 
-window = sg.Window("Stone-Paper-Scissor-APP", layout=layout, size= (300, 200), element_justification='c')
+window = sg.Window("Stone-Paper-Scissor-APP", layout=layout, size= (400, 200), element_justification='c')
 while True:
     window.read()
 
