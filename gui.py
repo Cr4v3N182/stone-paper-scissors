3397bcc073f20cc796416ac71fbbb981d7c8b6ea
i5-PC <mcienkowski@gmail.com>
2023-08-24 21:47:50 +0200

    Adding the justification to window elements

--- gui.py
+++ gui.py
@@ -3,14 +3,14 @@ import PySimpleGUI as sg
 sg.theme("DarkAmber")
 
 label = sg.Text("Choose stone, paper or scissors")
-stone_button = sg.Button("Stone", key="stone", )
+stone_button = sg.Button("Stone", key="stone")
 paper_button = sg.Button("Pape", key="paper")
 scissors_button = sg.Button("Scissors", key="scissors")
 
 layout = [[label], [[stone_button, paper_button, scissors_button]]]
 
-window = sg.Window("Stone-Paper-Scissor-APP", layout=layout, size= (300, 200))
-
-window.read()
+window = sg.Window("Stone-Paper-Scissor-APP", layout=layout, size= (300, 200), element_justification='c')
+while True:
+    window.read()
 
 window.close()
