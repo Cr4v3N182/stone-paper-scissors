01ec7dfbfcfffdd9720be0061439b648a45be424
i5-PC <mcienkowski@gmail.com>
2023-08-24 21:40:23 +0200

    Implementing ui, label, buttons and root window

---
+++ gui.py
@@ -0,0 +1,14 @@
+import PySimpleGUI as sg
+
+label = sg.Text("Choose stone, paper or scissors")
+stone_button = sg.Button("Stone", key="stone", )
+paper_button = sg.Button("Pape", key="paper")
+scissors_button = sg.Button("Scissors", key="scissors")
+
+layout = [[label], [[stone_button, paper_button, scissors_button]]]
+
+window = sg.Window("Stone-Paper-Scissor-APP", layout=layout, size= (300, 200))
+
+window.read()
+
+window.close()
