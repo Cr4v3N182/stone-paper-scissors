1bbf3ab6bbcfbf437be6976eabbdfe1cd0ac4620
i5-PC <mcienkowski@gmail.com>
2023-08-25 08:19:48 +0200

    Implement show_score_button and exit_button - without an instructions

--- gui.py
+++ gui.py
@@ -11,9 +11,11 @@ label = sg.Text("Choose stone, paper or scissors")
 stone_button = sg.Button("Stone", key="stone")
 paper_button = sg.Button("Paper", key="paper")
 scissors_button = sg.Button("Scissors", key="scissors")
+show_score_button = sg.Button("Show score", key="score")
+exit_button = sg.Button("Exit", key="exit")
 result = sg.Text("", key="output")
 
-layout = [[label], [[stone_button, paper_button, scissors_button]]]
+layout = [[label], [[stone_button, paper_button, scissors_button]],[show_score_button, result], [exit_button]]
 
 window = sg.Window("Stone-Paper-Scissor-APP", layout=layout, size= (400, 200), element_justification='c')
 while True:
